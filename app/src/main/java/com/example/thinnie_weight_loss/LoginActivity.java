7bb33e3b6aceae5b6836e6a3beeca1d1df2065b7
yonitg139 <yonitg139@gmail.com>
2020-08-29 13:12:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/thinnie_weight_loss/LoginActivity.java
@@ -0,0 +1,99 @@
+package com.example.thinnie_weight_loss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /// values we'll get from the digital scale
+    Integer weight_value;
+    double first_weight = 150;
+    double bmi_value = 34.6;
+    int loss_value;
+    int percent_value;
+    int fat_value = 28;
+
+    String id;
+    String name;
+    TextView fat;
+    TextView bmi;
+    TextView percent;
+    TextView loss;
+    TextView weight;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //Read from shared preferences
+        SharedPreferences sharedPreferences = getSharedPreferences(MainActivity.sharedName, 0);
+        weight_value = sharedPreferences.getInt(MainActivity.WEIGHT, 0);
+        id = sharedPreferences.getString(MainActivity.ID, null);
+        name = sharedPreferences.getString(MainActivity.USER_NAME, null);
+        percent_value = (int)Math.ceil(weight_value/first_weight*100);
+        loss_value = (int)Math.ceil(first_weight - weight_value);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        fat = findViewById(R.id.fatData);
+        loss = findViewById(R.id.weightLoss);
+        percent = findViewById(R.id.weightPercent);
+        bmi = findViewById(R.id.BMIData);
+        weight = findViewById(R.id.CurrentWeight);
+
+        //show values once without saving in DB
+        weight.setText(weight_value.toString());
+        bmi.setText(String.valueOf((int) bmi_value));
+        percent.setText(String.valueOf(percent_value));
+        loss.setText(String.valueOf(loss_value));
+        fat.setText(String.valueOf(fat_value));
+
+        String url = "https://talez.mtacloud.co.il/includes/app/weight_update.php";
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT). show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, "Failed to update", Toast.LENGTH_SHORT). show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("Id", id);
+                params.put("weight", weight_value.toString());
+                return params;
+            }
+        };
+        queue.add(request);
+
+    }
+}
---
+++ app/src/main/java/com/example/thinnie_weight_loss/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.thinnie_weight_loss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String LOGGED_IN_PREFERENCE = "loggedInPreference";
+    public static final String LOGGED_IN_KEY = "loggedInKey";
+
+    public static final String sharedName = "shared_file";
+    public final static String WEIGHT = "weight_value";
+    public final static String ID = "id";
+    public final static String USER_NAME = "user_name";
+    private EditText username;
+    private EditText password;
+    CheckBox loginState;
+    Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences prefs = getSharedPreferences(LOGGED_IN_PREFERENCE, MODE_PRIVATE);
+        boolean loggedIn = prefs.getBoolean(LOGGED_IN_KEY, false);
+        if(loggedIn) {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.userName);
+        password = findViewById(R.id.Password);
+        loginState = findViewById(R.id.remmeber);
+        login = findViewById(R.id.login);
+    }
+
+    public void login(View view) throws IOException {
+        String url = "https://talez.mtacloud.co.il/includes/app/user_login.php";
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String result = jsonObject.getString("response");
+
+
+                    if (result.equals("ok")){
+                        String Id = jsonObject.getString("id");
+                        SharedPreferences sharedPreferences = getSharedPreferences(sharedName, 0);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt(WEIGHT, 100);
+                        editor.putString(ID, Id);
+                        editor.commit();
+
+//                        SharedPreferences loggedInPreference = getSharedPreferences(LOGGED_IN_PREFERENCE, 0);
+//                        SharedPreferences.Editor loggedInEditor = loggedInPreference.edit();
+//                        editor.putBoolean(LOGGED_IN_KEY, loginState.isChecked());
+//                        editor.commit();
+
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                    else if (result.equals("nouser")){
+                        Toast.makeText(MainActivity.this, "Wrong User", Toast.LENGTH_SHORT). show();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Wrong Password", Toast.LENGTH_SHORT). show();
+                    }
+
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, "Failed to parse response", Toast.LENGTH_SHORT). show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, "Failed to connect", Toast.LENGTH_SHORT). show();
+                }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("username", username.getText().toString());
+                params.put("password", password.getText().toString());
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+}
